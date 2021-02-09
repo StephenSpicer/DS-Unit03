9820d3d0c108444162757469deb741880cc625cf
StephenSpicer <slupsha@gmail.comm>
2021-02-08 20:16:57 -0500

    function01

--- lambdata/example_module.py
+++ lambdata/example_module.py
@@ -2,3 +2,17 @@
 
 favorite_animals = ['tree frog', 'white rhino', 'zebra']
 
+nullset = ['Nan', 1, 2, 3, 4]
+
+nulldf = pd.DataFrame(nullset)
+
+
+ODI_runs = {'name': ['Tendulkar', 'Sangakkara', 'Ponting',
+                      'Jayasurya', 'Jayawardene', 'Kohli',
+                      'Haq', 'Kallis', 'Ganguly', 'Dravid'],
+            'runs': [18426, 14234, 13704, 13430, 12650,
+                     11867, 11739, 11579, 11363, 10889]}
+
+ODIdf = pd.DataFrame(ODI_runs)
+
+ODIdf.iloc[,1] = np.NaN
--- lambdata/help_functions.py
+++ lambdata/help_functions.py
@@ -1,2 +1,17 @@
 # find nulls in dataframe
 
+
+# a function to return a null count
+def null_count(df):
+    nullcount = df.isnull().sum()
+    return nullcount
+
+
+# a function to randomize a dataframe
+# def randomize(df, seed):
+    # randomizer = 
+
+# def train_test_split(df, frac=0.8):
+#    train = df.len()*frac
+#    test = df.len()*(1-frac)
+#    return
